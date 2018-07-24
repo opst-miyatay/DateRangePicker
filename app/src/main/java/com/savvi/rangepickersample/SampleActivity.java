56b704515bcdc3102ae159b69ba645f84268b9ec
宮田友美 <yumi_miyata@gdo.co.jp>
2018-07-24 12:36:07 +0900

    ・選択可能範囲の最後の日を選択できない不具合修正 ・ソースコード整理

--- app/src/main/java/com/savvi/rangepickersample/SampleActivity.java
+++ app/src/main/java/com/savvi/rangepickersample/SampleActivity.java
@@ -44,47 +44,12 @@ public class SampleActivity extends AppCompatActivity {
         nextYear.add(Calendar.MONTH, 6);
 
         lastYear = Calendar.getInstance();
-//        lastYear.add(Calendar.YEAR, -10);
 
         calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
         button = (Button) findViewById(R.id.get_selected_dates);
 
         ArrayList<Date> selected = new ArrayList<Date>();
-//        try {
-//            selected.add(new SimpleDateFormat("yyyy-MM-dd").parse("2018-09-01"));
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
         init(lastYear, nextYear, selected, new ArrayList<Integer>(), true);
-        /*
-        ArrayList<Integer> list = new ArrayList<>();
-//        list.add(1);// 日曜
-//        list.add(7);// 土曜
-
-        calendar.deactivateDates(list);
-        ArrayList<Date> arrayList = new ArrayList<>();
-        try {
-            SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
-            String strdate = "20-9-2018";
-            String strdate2 = "26-9-2018";
-            Date newdate = dateformat.parse(strdate);
-            Date newdate2 = dateformat.parse(strdate2);
-            arrayList.add(newdate);
-            arrayList.add(newdate2);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-//        try {
-            calendar.init(lastYear.getTime(), nextYear.getTime(), new SimpleDateFormat("yyyy年MM月", Locale.getDefault())) //
-                    .inMode(CalendarPickerView.SelectionMode.RANGE) //
-//                    .withSelectedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-01"))// デフォルト選択の日付
-                    .withSelectedDates(arrayList)
-                    .withDeactivateDates(list);// 選択できない曜日
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//                .withHighlightedDates(arrayList);
-*/
 
         Button all = (Button) findViewById(R.id.all);
         all.setOnClickListener(new View.OnClickListener() {
@@ -125,10 +90,10 @@ public class SampleActivity extends AppCompatActivity {
                 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
                 List<Date> dates = calendar.getSelectedDates();
                 StringBuilder builder = new StringBuilder();
-                for (Date date: dates) {
-                        builder.append(format.format(date)+",");
+                for (Date date : dates) {
+                    builder.append(format.format(date) + ",");
                 }
-                Log.d("list", builder.substring(0,builder.length()));
+                Log.d("list", builder.substring(0, builder.length()));
 
             }
         });
@@ -141,17 +106,11 @@ public class SampleActivity extends AppCompatActivity {
         if (dates == null || dates.length <= 0) {
             return arrayList;
         }
-//        try {
-//            SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
         for (Date date : dates) {
             if (date != null) {
-//                Date newdate = dateformat.parse(date);
                 arrayList.add(date);
             }
         }
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
         return arrayList;
     }
 
@@ -167,7 +126,9 @@ public class SampleActivity extends AppCompatActivity {
         calendar.setActiveMax(null);
         selected = false;
     }
+
     boolean selected;
+
     private void init(Calendar last, Calendar next, final List<Date> selectedDates, final List<Integer> deactivedates, boolean scroll) {
         // 祝日
         SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
@@ -181,53 +142,37 @@ public class SampleActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
-
-//        if (activeMin != null) {
-            calendar.init(last.getTime(), next.getTime(), TimeZone.getTimeZone("Asia/Tokyo"), Locale.getDefault(), new SimpleDateFormat("yyyy年MM月"), activeMin, activeMax) //
-                    .inMode(CalendarPickerView.SelectionMode.RANGE) //
+        // カレンダーの初期化
+        calendar.init(last.getTime(), next.getTime(), TimeZone.getTimeZone("Asia/Tokyo"), Locale.getDefault(), new SimpleDateFormat("yyyy年MM月"), activeMin, activeMax) //
+                .inMode(CalendarPickerView.SelectionMode.RANGE) //
 //                    .withSelectedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-01"))// デフォルト選択の日付
-                    .setHoliday(holidayList)
-                    .withDeactivateDates(deactivedates)
-                    .withSelectedDates(selectedDates, scroll);
-//        } else {
-//            calendar.init(last.getTime(), next.getTime(), new SimpleDateFormat("yyyy年MM月", Locale.getDefault())) //
-//                    .inMode(CalendarPickerView.SelectionMode.RANGE) //
-////                    .withSelectedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-01"))// デフォルト選択の日付
-//                    .setHoliday(holidayList)
-//                    .withDeactivateDates(deactivedates)
-//                    .withSelectedDates(selectedDates, scroll);
-////                .setShortWeekdays(weeks)
-//            // 選択できない曜日
-//        }
+                .setHoliday(holidayList)
+                .withDeactivateDates(deactivedates)
+                .withSelectedDates(selectedDates, scroll);
 
+        // 日付選択時の処理
         calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
             @Override
             public void onDateSelected(MonthCellDescriptor cell) {
-//                calendar.set
-
                 switch (cell.getRangeState()) {
                     case FIRST:
                     case LAST:
-                        Log.d("hoge", cell.getRangeState() + " " + cell.getDate());
                         selected = true;
                         selectLast = cell.getDate();
                         calendar.setActiveMin(null);
                         calendar.setActiveMax(null);
                         activeMin = null;
                         activeMax = null;
-//                        init(lastYear, nextYear, selectedDates, SampleActivity.this.deactivedates, false);
-
                         break;
                     case MIDDLE:
-                        Log.d("hoge", "MIDDLE " + cell.getDate());
                         break;
                     case NONE:
-                        Log.d("hoge", "NONE " + cell.getDate());
-
                         if (selected) {
+                            // 範囲選択中の場合は、範囲選択、選択可能範囲をクリアする
                             clear();
                             init(lastYear, nextYear, new ArrayList<Date>(), SampleActivity.this.deactivedates, false);
                         } else {
+                            // 範囲選択していない最初のタップ
                             selectFirst = cell.getDate();
                             // 未選択の状態で選択された場合、31日より先を選択不可とする
                             Date date = cell.getDate();
@@ -247,11 +192,6 @@ public class SampleActivity extends AppCompatActivity {
                             List<Date> selectedDates = new ArrayList<>();
                             selectedDates.add(date);
 
-//                            List<Date> from = createList(lastYear.getTime(), activeMin);
-//                            List<Date> to = createList(activeMax, nextYear.getTime());
-//                            deactivedates.addAll(from);
-
-
                             // 選択不可曜日
                             init(lastYear, nextYear, selectedDates, SampleActivity.this.deactivedates, false);
                         }
@@ -259,38 +199,14 @@ public class SampleActivity extends AppCompatActivity {
                     default:
                         throw new RuntimeException();
                 }
-//                if (cell.getRangeState() == RangeState.NONE) {
-//                } else if (cell.getRangeState() == RangeState.LAST) {
-////                    activeMax = null;
-////                    activeMin = null;
-////                    init(lastYear, nextYear, selectedDates, SampleActivity.this.deactivedates);
-//                }
-                Log.d("sample", "******* rangeState=" + cell.getRangeState() + "onDateSelected:" + cell.getDate());
             }
 
             @Override
             public void onDateUnselected(MonthCellDescriptor cell) {
                 selectFirst = null;
                 selectLast = null;
-                Log.d("sample", "******* rangeState=" + cell.getRangeState() + "onDateUnselected:" + cell.getDate());
             }
         });
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//                .withHighlightedDates(arrayList);
     }
 
-//    private List<Date> createList(Date from, Date to) {
-//        List<Date> list = new ArrayList<>();
-//        Date date = from;
-//        while(from.compareTo(to) ==0) {
-//            list.add(date);
-//            Calendar cal = Calendar.getInstance();
-//            cal.setTime(date);
-//            cal.add(Calendar.DATE, 1);
-//            date = cal.getTime();
-//        }
-//        return list;
-//    }
 }
